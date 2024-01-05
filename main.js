5a10533e81eac2e705b7384818ce83de76032c14
nattdev <110750471+nattdev@users.noreply.github.com>
2024-01-05 00:50:35 -0500

    Add dynamic text into date element

--- main.js
+++ main.js
@@ -2,9 +2,11 @@
 
 let latitude;
 let longitude;
+let timestamp;
 
 let latitudeElement = document.querySelector("#latitude");
 let longitudeElement = document.querySelector("#longitude");
+let dateElement = document.querySelector("#date");
 
 /** Fetch API **/
 
@@ -26,6 +28,7 @@ fetch(apiUrl)
     console.log(data);
 		latitude = parseFloat(data["iss_position"]["latitude"]);
 		longitude = parseFloat(data["iss_position"]["longitude"]);
+		timestamp = data["timestamp"];
     // Aquí puedes realizar acciones adicionales con los datos
 		latitudeElement.innerText = latitude;
 		longitudeElement.innerText = longitude;
@@ -38,7 +41,27 @@ fetch(apiUrl)
 
 		var marker = L.marker([latitude, longitude]).addTo(map);
 
-  })
+	// Crear un objeto Date con el timestamp (multiplicando por 1000 para convertir a milisegundos)
+	const date = new Date(timestamp * 1000);
+	console.log(timestamp);
+	// Obtener las partes de la fecha
+	const year = date.getFullYear();
+	const month = ("0" + (date.getMonth() + 1)).slice(-2);  // Los meses van de 0 a 11, por eso se suma 1
+	const day = ("0" + date.getDate()).slice(-2);
+	const hours = ("0" + date.getHours()).slice(-2);
+	const minutes = ("0" + date.getMinutes()).slice(-2);
+	const seconds = ("0" + date.getSeconds()).slice(-2);
+
+	// Formatear la fecha en un formato legible
+	const formattedDate = `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
+
+	dateElement.innerText = formattedDate;
+
+// Mostrar el resultado
+console.log(formattedDate);	
+
+
+		 })
   .catch(error => {
     // Manejar errores de red o errores en la API
     console.error('Error al obtener datos:', error);
